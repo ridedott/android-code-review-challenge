e6f9f881db088668048b5411cfb2b8699a4cc21e
Serhii Zabelnykov <serhii@ridedott.com>
2021-12-06 16:35:34 +0100

    initial commit

--- app/src/main/java/com/ridedott/dottapp/details/TodoDetailsFragment.kt
+++ app/src/main/java/com/ridedott/dottapp/details/TodoDetailsFragment.kt
@@ -5,11 +5,15 @@ import android.view.View
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.lifecycleScope
 import com.ridedott.dottapp.R
 import com.ridedott.dottapp.repository.TodoItem
 import com.ridedott.dottapp.repository.TodoItemId
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class TodoDetailsFragment(val todoItem: TodoItem) : Fragment(R.layout.todo_details_fragment) {
 
@@ -18,5 +22,18 @@ class TodoDetailsFragment(val todoItem: TodoItem) : Fragment(R.layout.todo_detai
         view.findViewById<TextView>(R.id.title).text = todoItem.title
         view.findViewById<TextView>(R.id.description).text = todoItem.description
         view.findViewById<TextView>(R.id.subdescription).text = todoItem.subdescription
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val client = OkHttpClient()
+            val request = Request.Builder()
+                .url("https://jsonplaceholder.typicode.com/todos/${todoItem.id.value}")
+                .build()
+
+            val result = client.newCall(request).execute().body?.string()
+
+            launch(Dispatchers.Main) {
+                view.findViewById<TextView>(R.id.dataFromBackend).text = result
+            }
+        }
     }
 }
